dd59cca0f5695d840037264108c67a0c97401570
Matan-Giladi <136958960+Matan-Giladi@users.noreply.github.com>
2025-01-22 01:08:27 +0200

    Remember to install the chosen SM's Python package

---
+++ setup/setup_container.py
@@ -0,0 +1,5 @@
+# TODO:
+# 1. Get SECRET_MANAGER
+# 2. Save it to src/settings.py
+# 3. Install its Python dependency:
+#    Call setup/secret_managers/configure_cli.manage_secret_manager_dependency(manager: str)
